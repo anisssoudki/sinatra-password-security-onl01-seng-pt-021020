666f4781811dc5e28ecdcfadf7eb999f1320b96e
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-21 19:37:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,8 +38,8 @@ end
 	post "/login" do
   user = User.find_by(:username => params[:username])
  
-  if 
-    current_user = user.id
+  if user && user.authenticate(params[:password]) 
+    session[:user_id] = user.id
     redirect "/success"
   else
     redirect "/failure"
